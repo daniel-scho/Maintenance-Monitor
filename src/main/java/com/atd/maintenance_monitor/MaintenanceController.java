ee878ede6471d13b389cbba610584e15dfb70184
daniel-scho <bs.daniel.scho@gmail.com>
2022-06-17 13:59:12 +0200

    changeState and reset to red

---
+++ src/main/java/com/atd/maintenance_monitor/MaintenanceController.java
@@ -0,0 +1,36 @@
+package com.atd.maintenance_monitor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MaintenanceController {
+
+    // http://localhost:8080/sub/5/4
+    @GetMapping("/change/{message}")
+    public String changeState(
+            @PathVariable String message
+
+    ) {
+        if (message.equals("reset")) {
+            return "red";
+        } else {
+            return "green";
+        }
+    }
+
+    /*  Example Code:
+    // http://localhost:8080/sub?a=5&b=4
+    @GetMapping("/sub")
+    public int sub(
+            @RequestParam int a, @RequestParam int b
+    ) {
+        return a - b;
+    }
+    @GetMapping("/mal")
+    public int mal(
+            @RequestParam int a, @RequestParam int b
+    ) {
+        return a * b;
+    }
+    */
+
+}
