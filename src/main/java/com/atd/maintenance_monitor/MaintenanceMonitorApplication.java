96220148975d8443196647ff1d554fbfab25f368
daniel-scho <bs.daniel.scho@gmail.com>
2022-06-17 13:13:36 +0200

    initial commit

---
+++ src/main/java/com/atd/maintenance_monitor/MaintenanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.atd.maintenance_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atd/maintenance_monitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atd.maintenance_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
